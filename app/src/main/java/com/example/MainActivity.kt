f47ded94b9f7d375346e2611ae8a3a09fabab753
yanqi17118357 <3224848830@qq.com>
2023-06-20 17:08:45 +0800

    chore: finish first app create

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ui.theme.AndroidstudyphoneTheme
 
 class MainActivity : ComponentActivity() {
-    // 代码开始执行的位置
+    // 应用入口
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 通过可组合函数定义布局
         setContent {
             AndroidstudyphoneTheme {
                 // A surface container using the 'background' color from the theme
@@ -27,18 +31,25 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// 拥有Composable注解，可以用于setContent函数或者其他可组合的函数
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+    // Surface容器有color参数
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = Modifier.padding(24.dp)
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
+// 使用Preview注解可以看见当前应用的外观
+// showBackground参数设置为true的话会有白色的背景
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     AndroidstudyphoneTheme {
-        Greeting("Android")
+        Greeting("YanQi")
     }
 }
