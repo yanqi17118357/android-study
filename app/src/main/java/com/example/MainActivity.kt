4c211e6d1efcda8802a693f3a00a0dc3cc54d34f
yanqi17118357 <3224848830@qq.com>
2023-06-20 16:14:47 +0800

    feat: project init

---
+++ app/src/main/java/com/example/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ui.theme.AndroidstudyphoneTheme
+
+class MainActivity : ComponentActivity() {
+    // 代码开始执行的位置
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidstudyphoneTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidstudyphoneTheme {
+        Greeting("Android")
+    }
+}
